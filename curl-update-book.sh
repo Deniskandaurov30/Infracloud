5294d3ca47d5b7642dcb025a3aed54843d2a878d
NetAcad DEVASC <changeme@example.com>
2025-10-23 12:42:46 +0000

    update

---
+++ curl-update-book.sh
@@ -0,0 +1,5 @@
+curl -X PUT "http://library.demo.local/api/v1/books/1" \
+-H "accept: application/json" \
+-H "X-API-KEY: cisco|_B3_GQTNmTR67mnoJVv879Iw2px4euIwx7m5rIJixNs" \
+-H "Content-Type: application/json" \
+-d "{ \"id\": 0, \"title\": \"Learning\", \"author\": \"Erling\"}"
